42d2c2b52a5a72adcf2c750681c80fc624fd3369
dillongraveline <dillon.graveline@gmail.com>
2020-10-16 22:02:29 -0400

    test

--- prophet.py
+++ prophet.py
@@ -1,4 +1,18 @@
 import pandas as pd
-import prophet as pt
+from fbprophet import Prophet
 
-gold_data = pd.read_csv(monthly_csv.csv)
+gold_data = pd.read_csv('monthly_csv.csv')
+
+model = Prophet()
+
+gold_data.rename(columns={'Date': 'ds', 'Price': 'y'}, inplace=True)
+
+model.fit(gold_data)
+
+future = model.make_future_dataframe(periods=12)
+
+forecast = model.predict(future)
+
+fig1 = model.plot(forecast)
+
+forecast.to_csv("output.csv")
